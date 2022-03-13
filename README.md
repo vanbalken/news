0bd370b950217e36dcb6eb0bd8f4f9a51b7deaf5
Arjan van Balken <arjanvanbalken@gmail.com>
2022-03-13 09:01:53 +0100

    Add new feeds

---
+++ README.md
@@ -0,0 +1,13 @@
+# News
+
+Simple website showing the lastest *n* articles from a selection of news outlets.
+
+Sources are queried on page request. The application has only one user and retrieving the data is quick enough.
+
+## Development
+
+```bash
+$ export FLASK_APP=news
+$ export FLASK_ENV=development
+$ flask run
+```
--- news/const.py
+++ news/const.py
@@ -3,7 +3,10 @@ __version__ = "0.1.0"
 URLS = [
     "http://feeds.nos.nl/nosnieuwsalgemeen",
     "http://feeds.bbci.co.uk/news/rss.xml",
+    "https://www.nu.nl/rss/Algemeen",
+    "https://www.aljazeera.com/xml/rss/all.xml",
     "https://news.ycombinator.com/rss",
     "http://feeds.feedburner.com/tweakers/nieuws",
+    "https://old.reddit.com/r/all.rss",
 ]
 ENTRIES_PER_FEED = 10
